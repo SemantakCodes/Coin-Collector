d11fb710785fd4f87db06dac8983f4479fa29c2b
SemantakCodes <semantak.bhandari10@gmail.com>
2026-03-25 14:29:04 +0545

    feat : ground completed

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -5,14 +5,15 @@ public class Ground : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float GroundSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector3.right * GroundSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * GroundSpeed * Time.deltaTime);
+
+        if(transform.position.x > 30f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/GroundManager.cs
+++ Assets/Scripts/GroundManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GroundManager : MonoBehaviour
@@ -8,13 +9,26 @@ public class GroundManager : MonoBehaviour
     [SerializeField] private GameObject Obstacles;
     [SerializeField] private Transform Player;
     [SerializeField] private GameObject Ground;
-
+    [SerializeField] private float groundOnScreen;
+    private float timer;
+    [SerializeField] private float timeDelay;
+    private float groundLength;
     private void SpawnGround(GameObject groundObject, GameObject OriginPoint)
     {
-        Instantiate(Ground, OriginPoint.transform.position, Quaternion.identity);
+        if(timer > timeDelay)
+        {
+            Instantiate(groundObject, OriginPoint.transform.position, Quaternion.identity);
+            timer = 0f;
+        }
     }
     private void Update()
     {
+        timer += Time.deltaTime;
         SpawnGround(Ground, spawnPoint);
+
+    }
+    private void Start()
+    {
+        
     }
 }
